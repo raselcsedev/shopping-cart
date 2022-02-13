a7d238f690005a83a979636d34eca50ca5f12087
raselcsedev <raselcse.dev@gmail.com>
2022-02-13 09:08:44 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,76 @@
+function updateProductNumber(product, price, isIncreasing){
+    const productInput = document.getElementById(product+'-number');
+    let productNumber = productInput.value;
+    if(isIncreasing == true){
+        productNumber=parseInt(productNumber)+1;
+    }
+    else if(productNumber>0){
+        productNumber=parseInt(productNumber)-1;
+    }
+    productInput.value = productNumber;
+    // update total
+    const productTotal = document.getElementById(product+'-total');
+     productTotal.innerText=productNumber*price;
+
+    //  update calculate
+     calculateTotal();
+}
+// handle calculate 
+function getInputValue(product){
+    const productInput = document.getElementById(product +'-number');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+}
+function calculateTotal(){
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal+caseTotal;
+    const tax = subTotal / 10;
+    const total = subTotal+tax;
+
+    // update on the HTML
+    document.getElementById('sub-total').innerText=subTotal;
+    document.getElementById('tax-amount').innerText=tax;
+    document.getElementById('total-price').innerText=total;
+}
+
+
+
+//increasing decreasing Phone number events
+document.getElementById('phone-plus').addEventListener('click', function(){
+    /* 
+    const productInput = document.getElementById('phone-number');
+    let productNumber = productInput.value;
+    productInput.value=parseInt(productNumber)+1; 
+    */
+    updateProductNumber('phone', 1219, true);
+})
+
+document.getElementById('phone-minus').addEventListener('click', function(){
+    /* 
+    const productInput = document.getElementById('phone-number');
+    let productNumber=productInput.value;
+    productInput.value = parseInt(productNumber)-1; 
+    */
+    updateProductNumber('phone', 1219, false);
+})
+
+
+
+//increasing decreasing Case number events
+document.getElementById('case-plus').addEventListener('click', function(){
+    /* 
+    const caseInput = document.getElementById('case-number');
+    const caseNumber = caseInput.value;
+    caseInput.value= parseInt(caseNumber)+1; 
+    */
+    updateProductNumber('case', 59, true )
+});
+document.getElementById('case-minus').addEventListener('click', function(){
+    /* 
+    const caseInput = document.getElementById('case-number');
+    const caseNumber= caseInput.value;
+    caseInput.value = parseInt(caseNumber) -1; 
+    */
+    updateProductNumber('case', 59, false);
+});
